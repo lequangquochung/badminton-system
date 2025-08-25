e97f3f37fb3ecba057b5b9aea36d947e6cf2b6d7
hunglqq <lequangquochung1912@gmail.com>
2025-08-25 16:16:33 +0700

    config cors

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,19 @@ import { errorHandler } from './middlewares/errorHandler';
 import authRouter from './routers/authRoutes';
 import matchesRoutes from './routers/matchesRoutes';
 import playersRoutes from './routers/playerRoutes';
-import cors from 'cors';
+
 const app = express();
 
-// config cors
-const corsOptions = {
-    origin: '*', // Allow all origins
-    optionsSuccessStatus: 200, // For legacy browser support,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-};
+// // config cors
+// const corsOptions = {
+//     origin: '*', // Allow all origins
+//     optionsSuccessStatus: 200, // For legacy browser support,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// };
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({
