0ea888a962ec9780d4ae80ac2c754d05bfb6a500
hunglqq <lequangquochung1912@gmail.com>
2025-08-25 16:30:05 +0700

    config cors

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,21 @@ import express from 'express';
 import { errorHandler } from './middlewares/errorHandler';
 import authRouter from './routers/authRoutes';
 import matchesRoutes from './routers/matchesRoutes';
+import cors from 'cors';
 import playersRoutes from './routers/playerRoutes';
 
 const app = express();
 
-// // config cors
-// const corsOptions = {
-//     origin: '*', // Allow all origins
-//     optionsSuccessStatus: 200, // For legacy browser support,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"]
-// };
+// config cors
+const corsOptions = {
+    origin: '*', // Allow all origins
+    optionsSuccessStatus: 200, // For legacy browser support,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+};
 
-// app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -29,5 +30,6 @@ app.use('/api/players', playersRoutes);
 app.use('/api/auth', authRouter);
 
 app.use(errorHandler);
+
 
 export default app;
