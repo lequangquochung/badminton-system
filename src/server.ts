931e2dfc8726a7bce3732787358a796869b029da
hunglqq <lequangquochung1912@gmail.com>
2025-08-25 11:05:26 +0700

    config cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,19 @@ import { log } from 'console';
 import app from './app';
 import ENV from './config/env.config';
 import { connectDatabase } from './config/database.config';
+import cors from 'cors';
 
 const PORT = ENV.SERVER_PORT;
 
 connectDatabase();
+
+// config cors
+const corsOptions = {
+  origin: '*', // Allow all origins
+  optionsSuccessStatus: 200 // For legacy browser support
+};
+
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   log('server is running');
