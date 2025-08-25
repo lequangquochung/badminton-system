e1cd554fd3b2f02a35c780aab13d32fb04b23af4
hunglqq <lequangquochung1912@gmail.com>
2025-08-25 11:14:14 +0700

    config cors

--- src/server.ts
+++ src/server.ts
@@ -11,11 +11,13 @@ connectDatabase();
 // config cors
 const corsOptions = {
   origin: '*', // Allow all origins
-  optionsSuccessStatus: 200 // For legacy browser support
+  optionsSuccessStatus: 200, // For legacy browser support,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 };
 
 app.use(cors(corsOptions));
-
+app.options("*", cors(corsOptions));
 app.listen(PORT, () => {
   log('server is running');
 });
